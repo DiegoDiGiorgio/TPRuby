a9e38ed503c59236f9caccbef7803410a05f1a10
Diego Di Giorgio <diego.i.digiorgio@gmail.com>
2021-10-30 18:05:46 -0300

    imprime en html por dia, falta dividir por horas y de ahi hacer la semana

--- lib/polycon/commands.rb
+++ lib/polycon/commands.rb
@@ -2,6 +2,7 @@ module Polycon
   module Commands
     autoload :Professionals, 'polycon/commands/professionals'
     autoload :Appointments, 'polycon/commands/appointments'
+    autoload :Writer, 'polycon/commands/writer'
     autoload :Version, 'polycon/commands/version'
     
 
@@ -22,6 +23,10 @@ module Polycon
       prefix.register 'show', Appointments::Show
       prefix.register 'cancel', Appointments::Cancel
       prefix.register 'cancel-all', Appointments::CancelAll
+    end
+
+    register 'writer', aliases: ['w'] do |prefix|
+      prefix.register 'write', Writer::Write
     end
 
     register 'version', Version, aliases: ['v', '-v', '--version']
